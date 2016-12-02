31fef42c71e9be1a4bc3e7b491915a4b6cc70aac
royredman4 <royredman4@gmail.com>
2016-12-01 17:42:02 -0800

    Added a print statement

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+	self.response.write('Comment Made')
 
 
 app = webapp2.WSGIApplication([
